5e9b8ae6f2b8f7ecddc68424088000fe53c83c09
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-03-21 10:14:27 -0500

    Add todos to JavaScript file

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+let thingsToDo = [
+    {
+        task: 'Clean room',
+        completed: true
+    }, {
+        task: 'Submit applications',
+        completed: false
+    }, {
+        task:
+            'Finish math homework',
+        completed: true
+    }, {
+        task: 'Finish algo homework',
+        completed: false
+    }, {
+        task: 'Finish 450 Homework',
+        completed: false
+    }]
+
 const todos = document.querySelectorAll('p')
 
 todos.forEach(function (todo) {
-    if (todo.textContent === 'the') {
+    if (todo.textContent.includes('the')) {
         todo.remove()
     }
-    console.log(todo.textContent)
 })
