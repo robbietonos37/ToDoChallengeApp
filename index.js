65ade07c2aa1c5152e8caa475d2d5616a8b70c54
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-03-25 20:06:50 -0500

    Add new input and filter function in js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let thingsToDo = [
+const thingsToDo = [
     {
         task: 'Clean room',
         completed: true
@@ -28,19 +28,58 @@ thingsToDo.forEach(function(todo)){
 }
 */
 
-const incompleteTodos = thingsToDo.filter(function (todo) {
-    return !todo.completed
-})
-const summaryStatement = document.createElement('p')
-summaryStatement.textContent = `You have ${incompleteTodos.length} items left to do`
-document.querySelector('body').appendChild(summaryStatement)
+// 1. Setup a div container for todos
+// 2. Setup filters (searchText) and wire up a new filter input to change it 
+// 3. Create a renderToDos function to render and rerender the latest filtered data
+
+
+// Starts
+
+const filters = {
+    searchText: ''
+}
+
+const renderToDos = function (ToDos, filters) {
+    const filteredToDos = ToDos.filter(function (todo) {
+        return todo.task.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    const incompleteTodos = filteredToDos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summaryStatement = document.createElement('p')
+    summaryStatement.textContent = `You have ${incompleteTodos.length} items left to do`
+    document.querySelector('#todos').appendChild(summaryStatement)
+
+
+
+    filteredToDos.forEach(function (todo) {
+        const todoEl = document.createElement('p')
+        todoEl.textContent = todo.task
+        document.querySelector('#todos').appendChild(todoEl)
+    })
+}
+
+renderToDos(thingsToDo, filters)
+
+
+
+
+
 // How many todos you have left (p element)
 // Add a p for each todo above
+/*
 thingsToDo.forEach(function (note) {
     const newTask = document.createElement('p')
     newTask.textContent = note.task
     document.querySelector('body').appendChild(newTask)
 })
+*/
+
+//Ends
 
 
 document.querySelector('#add-todo').addEventListener('click', function (event) {
@@ -49,4 +88,9 @@ document.querySelector('#add-todo').addEventListener('click', function (event) {
 
 document.querySelector('#new-todo-text').addEventListener('input', function (e) {
     console.log(e.target.value)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderToDos(thingsToDo, filters)
 })
