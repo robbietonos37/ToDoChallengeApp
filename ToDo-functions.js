39eb7af3a872243e36aaaaf292458e04126157da
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-04-01 15:14:38 -0500

    Refactor functions to new JS file

---
+++ ToDo-functions.js
@@ -0,0 +1,59 @@
+// Fetch existing todos from localStorage
+const getSavedToDos = function () {
+    const todoJSON = localStorage.getItem('thingsToDo')
+
+    if (todoJSON !== null) {
+        return JSON.parse(todoJSON)
+    } else {
+        return []
+    }
+}
+
+// Save todos in localStorage
+const saveToDos = function (thingsToDo) {
+    localStorage.setItem('thingsToDo', JSON.stringify(thingsToDo))
+}
+
+// Render application todos based on filters
+
+const renderToDos = function (ToDos, filters) {
+    const filteredToDos = ToDos.filter(function (todo) {
+        const searchTextMatch = todo.task.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+
+
+
+    const incompleteTodos = filteredToDos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    generateSummaryDOM(incompleteTodos)
+
+    document.querySelector('#todos').innerHTML = ''
+    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
+
+
+
+    filteredToDos.forEach(function (todo) {
+        generatetodosDOM(todo)
+        document.querySelector('#todos').appendChild(generatetodosDOM(todo))
+    })
+}
+
+// Get the DOM elements for an individual note
+const generatetodosDOM = function (todo) {
+    const todoEl = document.createElement('p')
+    todoEl.textContent = todo.task
+    return todoEl
+}
+
+// Get the DOM elements for list summary
+
+const generateSummaryDOM = function (incompleteTodos) {
+    const summaryStatement = document.createElement('p')
+    summaryStatement.textContent = `You have ${incompleteTodos.length} items left to do`
+    return summaryStatement
+}
--- index.js
+++ index.js
@@ -1,90 +1,11 @@
-// 1. Delete dummy data
-// 2. Read and parse the data when the app starts up
-// 3. Stringify and writie the data when new data
-
-let thingsToDo = [
-    /* {
-        task: 'Clean room',
-        completed: true
-    } */
-]
-
-/*This is how I did it and it still worked
-let numOfThingsTodo = 0
-thingsToDo.forEach(function(todo)){
-    if(!todo.completed){
-        numOfThingsTodo++;
-
-    }
-    //we just used this variable in our summarystatement
-}
-*/
-
-// 1. Setup a div container for todos
-// 2. Setup filters (searchText) and wire up a new filter input to change it 
-// 3. Create a renderToDos function to render and rerender the latest filtered data
-
-
-// Starts
+let thingsToDo = getSavedToDos()
 
 const filters = {
     searchText: '',
     hideCompleted: false
 }
 
-const todoJSON = localStorage.getItem('thingsToDo')
-
-if (todoJSON !== null) {
-    thingsToDo = JSON.parse(todoJSON)
-}
-
-const renderToDos = function (ToDos, filters) {
-    const filteredToDos = ToDos.filter(function (todo) {
-        const searchTextMatch = todo.task.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-
-        return searchTextMatch && hideCompletedMatch
-    })
-
-
-
-    const incompleteTodos = filteredToDos.filter(function (todo) {
-        return !todo.completed
-    })
-
-    document.querySelector('#todos').innerHTML = ''
-
-    const summaryStatement = document.createElement('p')
-    summaryStatement.textContent = `You have ${incompleteTodos.length} items left to do`
-    document.querySelector('#todos').appendChild(summaryStatement)
-
-
-
-    filteredToDos.forEach(function (todo) {
-        const todoEl = document.createElement('p')
-        todoEl.textContent = todo.task
-        document.querySelector('#todos').appendChild(todoEl)
-    })
-}
-
 renderToDos(thingsToDo, filters)
-
-
-
-
-
-// How many todos you have left (p element)
-// Add a p for each todo above
-/*
-thingsToDo.forEach(function (note) {
-    const newTask = document.createElement('p')
-    newTask.textContent = note.task
-    document.querySelector('body').appendChild(newTask)
-})
-*/
-
-//Ends
-
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
@@ -95,7 +16,7 @@ document.querySelector('#ToDo-form').addEventListener('submit', function (e) {
     e.preventDefault()
 
     thingsToDo.push({ task: e.target.elements.newToDo.value, completed: false })
-    localStorage.setItem('thingsToDo', JSON.stringify(thingsToDo))
+    saveToDos()
     renderToDos(thingsToDo, filters)
     //e.target.elements.newToDo.value = ''
 })
@@ -125,11 +46,3 @@ document.querySelector('#hide-completed').addEventListener('change', function (e
 })
 
 
-
-
-
-// 1. Create a form with a single input for todo text
-// 2. Setup a submit handler and cancel the default aciton
-// 3. Add a new item to the todos array with that text data
-// 4. Rerender the application
-// 5. clear the input field value
